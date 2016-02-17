9b462012cc70d8d104c390085ee7777b047c02e7
Jacob Penner <jkpenner91@gmail.com>
2016-02-16 20:54:47 -0800

    Move Tile Pick Up code from GameBoard in TilePickUpManager script

--- Assets/Scripts/GameBoard/GameBoard.cs
+++ Assets/Scripts/GameBoard/GameBoard.cs
@@ -15,7 +15,7 @@ public class GameBoard : Singleton<GameBoard>{
 
     public int boardWidth = 5;
     public int boardHeight = 5;
-    public Vector4 boardPadding = Vector4.zero;
+    public Vector2 boardSpacing = Vector2.zero;
 
     public GameBoardSlot[,] slots;
 
@@ -23,6 +23,7 @@ public class GameBoard : Singleton<GameBoard>{
         // Create instance of the selected tile object
         selectedTileObject = Instantiate(selectedTilePrefab);
         selectedTileObject.transform.SetParent(this.transform);
+        selectedTileObject.SetActive(false);
 
         // Set the size of the GameBoard object
         //RectTransform rTransform = GetComponent<RectTransform>();
@@ -33,7 +34,10 @@ public class GameBoard : Singleton<GameBoard>{
         // Create a sub group for all the tiles to be parented to
         var tileGroupGO = new GameObject("Tiles");
         tileGroupGO.transform.SetParent(this.transform);
-        tileGroupGO.transform.Translate(new Vector3(boardPadding.x, boardPadding.y));
+
+
+        float xMin = (boardWidth + (boardSpacing.x * (boardWidth - 1f))) / 2f;
+        float yMin = (boardHeight + (boardSpacing.y * (boardHeight - 1f))) / 2f;
 
         // Create the background slots for each tile
         slots = new GameBoardSlot[boardWidth, boardHeight];
@@ -45,80 +49,16 @@ public class GameBoard : Singleton<GameBoard>{
                 slots[x, y] = tileGO.GetComponent<GameBoardSlot>();
 
                 tileGO.transform.SetParent(tileGroupGO.transform);
-                tileGO.transform.localPosition = new Vector3(x - (boardWidth / 2f), y - (boardHeight / 2f), 0f);
+
+                tileGO.transform.localPosition = new Vector3(
+                    x + (boardSpacing.x * (x - 1)) - xMin, 
+                    y + (boardSpacing.y * (y - 1)) - yMin, 0f);
             }
             slots[x, 0].SlotTile = Instantiate(tilePrefab).GetComponent<Tile>();
             slots[x, 0].SlotTile.SetText("A");
             slots[x, 0].SlotTile.transform.position = slots[x, 0].transform.position;
         }
 	}
-
-    public GameBoardSlot lastSlot;
-    public Tile pickedUpTile;
-
-    public void Update() {
-        // On Mouse Down pick up tile if there is one
-        // under the mouse's current location
-        if (Input.GetMouseButtonDown(0)) {
-            var hitinfo = RaycastFromMouse();
-            if (hitinfo.collider != null) {
-                var boardSlot = hitinfo.transform.GetComponent<GameBoardSlot>();
-                if (boardSlot != null && boardSlot.SlotTile != null) {
-                    lastSlot = boardSlot;
-
-                    pickedUpTile = boardSlot.SlotTile;
-                    pickedUpTile.GetComponent<Animator>().SetTrigger("PickUp");
-                    
-                    boardSlot.SlotTile = null;
-                }
-            }
-        }
-
-        // On Mouse Up drop tile into open slot under
-        // the mouse or move it back to the last slot
-        if (Input.GetMouseButtonUp(0) && pickedUpTile != null) {
-            var hitinfo = RaycastFromMouse();
-            if (hitinfo.collider != null) {
-                var boardSlot = hitinfo.transform.GetComponent<GameBoardSlot>();
-                if (boardSlot != null && boardSlot.SlotTile == null) {
-                    boardSlot.SlotTile = pickedUpTile;
-
-                    pickedUpTile.transform.position = boardSlot.transform.position;
-                    pickedUpTile.GetComponent<Animator>().SetTrigger("Drop");
-
-                    lastSlot = null;
-                    pickedUpTile = null;
-                } else {
-                    ReturnTileToLastSlot();
-                }
-            } else {
-                ReturnTileToLastSlot();
-            }
-        }
-
-        if (pickedUpTile != null) {
-            var followPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            followPoint.z = pickedUpTile.transform.position.z;
-
-            pickedUpTile.transform.position = Vector3.Lerp(followPoint, 
-                pickedUpTile.transform.position, 10 * Time.deltaTime);
-        }
-    }
-
-    private RaycastHit2D RaycastFromMouse() {
-        var mousePosition = Input.mousePosition;
-        return Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-    }
-
-    private void ReturnTileToLastSlot() {
-        lastSlot.SlotTile = pickedUpTile;
-
-        pickedUpTile.transform.position = lastSlot.transform.position;
-        pickedUpTile.GetComponent<Animator>().SetTrigger("Drop");
-
-        lastSlot = null;
-        pickedUpTile = null;
-    }
 
     public void SetSelectedTile(Transform target) {
         if (selectedTileObject != null) {
---
+++ Assets/Scripts/Managers/TilePickUpManager.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TilePickUpManager : Singleton<TilePickUpManager> {
+    protected TilePickUpManager() { }
+
+    private GameBoardSlot _lastSlot;
+    private Tile _pickedUpTile;
+
+    public void Update() {
+        // On Mouse Down pick up tile if there is one
+        // under the mouse's current location
+        if (Input.GetMouseButtonDown(0)) {
+            var hitinfo = RaycastFromMouse();
+            if (hitinfo.collider != null) {
+                var boardSlot = hitinfo.transform.GetComponent<GameBoardSlot>();
+                if (boardSlot != null && boardSlot.SlotTile != null) {
+                    PickUpTile(boardSlot);
+                }
+            }
+        }
+
+        // On Mouse Up drop tile into open slot under
+        // the mouse or move it back to the last slot
+        if (Input.GetMouseButtonUp(0) && _pickedUpTile != null) {
+            var hitinfo = RaycastFromMouse();
+            if (hitinfo.collider != null) {
+                var boardSlot = hitinfo.transform.GetComponent<GameBoardSlot>();
+                if (boardSlot != null && boardSlot.SlotTile == null) {
+                    DropTile(boardSlot);
+                } else {
+                    ReturnTileToLastSlot();
+                }
+            } else {
+                ReturnTileToLastSlot();
+            }
+        }
+
+        // Updates the Following Tile towards the mouse's position
+        if (_pickedUpTile != null) {
+            var followPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            followPoint.z = _pickedUpTile.transform.position.z;
+
+            _pickedUpTile.transform.position = Vector3.Lerp(followPoint,
+                _pickedUpTile.transform.position, 10 * Time.deltaTime);
+        }
+    }
+
+    private RaycastHit2D RaycastFromMouse() { 
+        return Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+    }
+
+    private void ReturnTileToLastSlot() {
+        _lastSlot.SlotTile = _pickedUpTile;
+
+        _pickedUpTile.transform.position = _lastSlot.transform.position;
+        _pickedUpTile.GetComponent<Animator>().SetTrigger("Drop");
+
+        SetSortingLayerRecur(_pickedUpTile.transform, "Default");
+
+        _lastSlot = null;
+        _pickedUpTile = null;
+    }
+
+    private void PickUpTile(GameBoardSlot slot) {
+        _lastSlot = slot;
+
+        _pickedUpTile = slot.SlotTile;
+        _pickedUpTile.GetComponent<Animator>().SetTrigger("PickUp");
+
+        SetSortingLayerRecur(_pickedUpTile.transform, "PickUp");
+
+        slot.SlotTile = null;
+    }
+
+    private void DropTile(GameBoardSlot slot) {
+        slot.SlotTile = _pickedUpTile;
+
+        _pickedUpTile.transform.position = slot.transform.position;
+        _pickedUpTile.GetComponent<Animator>().SetTrigger("Drop");
+
+        SetSortingLayerRecur(_pickedUpTile.transform, "Default");
+
+        _lastSlot = null;
+        _pickedUpTile = null;
+    }
+
+    private void SetSortingLayerRecur(Transform obj, string layer) {
+        if (obj != null) {
+            var sRender = obj.GetComponent<Renderer>();
+            if (sRender != null) {
+                sRender.sortingLayerName = layer;
+            }
+
+            for (int i = 0; i < obj.childCount; i++) {
+                SetSortingLayerRecur(obj.GetChild(i), layer);
+            }
+        }
+    }
+}
