13ee514150006875fe44adbcdf35e56a1feb7378
Jacob Penner <jkpenner91@gmail.com>
2016-02-16 21:49:51 -0800

    Add Audio To Tile Prefabs

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,6 +5,10 @@ using System;
 
 public class Tile : MonoBehaviour, IPointerClickHandler, IDragHandler {
     public TextMesh textmesh;
+    public AudioSource audioSourc;
+    public AudioClip dropClip;
+    public AudioClip pickUpClip;
+    public AudioClip invalidDropClip;
 
     public void OnDrag(PointerEventData eventData) {
         throw new NotImplementedException();
@@ -17,6 +21,24 @@ public class Tile : MonoBehaviour, IPointerClickHandler, IDragHandler {
     public void SetText(string value) {
         if (textmesh != null) {
             textmesh.text = value;
+        }
+    }
+
+    public void OnPickUp() {
+        if (audioSourc != null) {
+            audioSourc.PlayOneShot(pickUpClip);
+        }
+    }
+
+    public void OnDrop() {
+        if (audioSourc != null) {
+            audioSourc.PlayOneShot(dropClip);
+        }
+    }
+
+    public void OnInvalidDrop() {
+        if (audioSourc != null) {
+            audioSourc.PlayOneShot(invalidDropClip);
         }
     }
 }
