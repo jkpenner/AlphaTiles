dea01a61b42da909e7ceba86d9ceb12710a127fa
Jacob Penner <jkpenner91@gmail.com>
2016-02-16 20:27:29 -0800

    Add Utility Scripts

---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+    static private T _instance;
+    static private T Instance {
+        get {
+            if (_instance == null) {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null) {
+                    GameObject singletonGO = new GameObject();
+                    singletonGO.name = "(Singleton)" + typeof(T).ToString();
+                    _instance = singletonGO.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/SortingLayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortingLayer : MonoBehaviour {
+    public string SortingLayerName = "Default";
+    public int SortingOrder = 0;
+
+    public void Awake() {
+        gameObject.GetComponent<MeshRenderer>().sortingLayerName = SortingLayerName;
+        gameObject.GetComponent<MeshRenderer>().sortingOrder = SortingOrder;
+    }
+}
