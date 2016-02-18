af53758708f8a8f264a14e97ed7712d25dbb8ce3
Jacob Penner <jkpenner91@gmail.com>
2016-02-17 20:20:54 -0800

    Update GameBoard to allow external scripts to generate board

--- Assets/Scripts/GameBoard/GameBoard.cs
+++ Assets/Scripts/GameBoard/GameBoard.cs
@@ -3,32 +3,61 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System;
 
-public class GameBoard : Singleton<GameBoard>{
-    protected GameBoard() { }
+public class GameBoard : MonoBehaviour{
+    public bool generateBoardOnStart = false;
 
-    public GameObject gameBoardSlotPrefab;
+    // All prefabs used
+    public GameObject prefabGameBoardSlot;
+    public GameObject prefabTileSelected;
 
-    public GameObject selectedTilePrefab;
+    // The active object to show the selected tile
     private GameObject selectedTileObject;
+    private Transform tileGroup;
 
-    public GameObject tilePrefab;
-
+    // The size of the board
     public int boardWidth = 5;
     public int boardHeight = 5;
+
+    // Spacing between each tile
     public Vector2 boardSpacing = Vector2.zero;
 
+    // References all tiles on the board
     public GameBoardSlot[,] slots;
 
-	void Start () {
+	void Awake () {
         // Create instance of the selected tile object
-        selectedTileObject = Instantiate(selectedTilePrefab);
+        selectedTileObject = Instantiate(prefabTileSelected);
         selectedTileObject.transform.SetParent(this.transform);
         selectedTileObject.SetActive(false);
 
         // Create a sub group for all the tiles to be parented to
-        var tileGroupGO = new GameObject("Tiles");
-        tileGroupGO.transform.SetParent(this.transform);
-        tileGroupGO.transform.localPosition = Vector2.zero;
+        tileGroup = new GameObject("Tiles").transform;
+        tileGroup.SetParent(this.transform);
+        tileGroup.localPosition = Vector2.zero;
+
+        if (generateBoardOnStart) {
+            GenerateBoard(boardWidth, boardHeight);
+        }
+	}
+
+    public void Update() {
+        // Gets the GameBoardSlot under the mouse and move the selected tile object to its position
+        var hitinfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hitinfo.collider != null && hitinfo.transform.GetComponent<GameBoardSlot>() != null) {
+            selectedTileObject.SetActive(true);
+            selectedTileObject.transform.position = hitinfo.transform.position;
+        } else {
+            selectedTileObject.SetActive(false);
+        }
+    }
+
+    public void GenerateBoard(int width, int height) {
+        // ToDo: Reuse old GameBoardSlot's to reduce
+        // amount of instantiating during generating board
+        DestroyBoard();
+
+        boardWidth = width;
+        boardHeight = height;
 
         // Calculate the min values for positions of tiles
         float xMin = (boardWidth + (boardSpacing.x * (boardWidth - 1f))) / 2f;
@@ -38,42 +67,39 @@ public class GameBoard : Singleton<GameBoard>{
         slots = new GameBoardSlot[boardWidth, boardHeight];
         for (int x = 0; x < boardWidth; x++) {
             for (int y = 0; y < boardHeight; y++) {
-                var tileGO = GameObject.Instantiate(gameBoardSlotPrefab);
-                tileGO.name = tileGO.name + (y * boardWidth + x);
-
+                var tileGO = Instantiate(prefabGameBoardSlot);
                 slots[x, y] = tileGO.GetComponent<GameBoardSlot>();
 
-                tileGO.transform.SetParent(tileGroupGO.transform);
-
+                tileGO.name = prefabGameBoardSlot.name + (y * boardWidth + x);
+                tileGO.transform.SetParent(tileGroup);
                 tileGO.transform.localPosition = new Vector3(
-                    x + (boardSpacing.x * (x - 1)) - xMin, 
+                    x + (boardSpacing.x * (x - 1)) - xMin,
                     y + (boardSpacing.y * (y - 1)) - yMin, 0f);
             }
-            slots[x, 0].SlotTile = Instantiate(tilePrefab).GetComponent<Tile>();
-            slots[x, 0].SlotTile.SetText(x.ToString());
-            slots[x, 0].SlotTile.transform.SetParent(slots[x, 0].transform);
-            slots[x, 0].SlotTile.transform.position = slots[x, 0].transform.position;
-        }
-	}
-
-    public void Update() {
-        var hitinfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-        if (hitinfo.collider != null) {
-            selectedTileObject.SetActive(true);
-            selectedTileObject.transform.position = hitinfo.transform.position;
-        } else {
-            selectedTileObject.SetActive(false);
         }
     }
 
-    public void SetSelectedTile(Transform target) {
-        if (selectedTileObject != null) {
-            if (target != null) {
-                selectedTileObject.SetActive(true);
-                selectedTileObject.transform.position = target.position;
-            } else {
-                selectedTileObject.SetActive(false);
+    public void DestroyBoard() {
+        if (slots != null) {
+            for (int x = 0; x < slots.GetLength(0); x++) {
+                for (int y = 0; y < slots.GetLength(1); y++) {
+                    Destroy(slots[x, y].SlotTile.gameObject);
+                    Destroy(slots[x, y].gameObject);
+                }
             }
+            slots = null;
+        }
+    }
+
+    public void SetTile(int x, int y, Tile tile) {
+        SetTile(slots[x, y], tile);
+    }
+
+    public void SetTile(GameBoardSlot slot, Tile tile) {
+        if (slot != null && tile != null) {
+            slot.SlotTile = tile;
+            slot.SlotTile.transform.SetParent(slot.transform);
+            slot.SlotTile.transform.position = slot.transform.position;
         }
     }
 }
