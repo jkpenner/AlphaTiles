080736230ecf60363df6aea50257ec5eaa8884f0
Jacob Penner <jkpenner91@gmail.com>
2016-02-18 10:10:28 -0800

    Add ScoreManager and related UI changes

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreManager : Singleton<ScoreManager> {
+    public class DifficultyInfo {
+        public int Correct { get; set; }
+        public int Incorrect { get; set; }
+    }
+
+    private Dictionary<int, DifficultyInfo> _difficultyInfos;
+    public Dictionary<int, DifficultyInfo> DifficultyInfos {
+        get {
+            if (_difficultyInfos == null) {
+                _difficultyInfos = new Dictionary<int, DifficultyInfo>();
+            }
+            return _difficultyInfos;
+        }
+    }
+
+    public void AddCorrect(int difficulty) {
+        if (!DifficultyInfos.ContainsKey(difficulty)) {
+            DifficultyInfos.Add(difficulty, new DifficultyInfo());
+            DifficultyInfos[difficulty].Correct = 0;
+            DifficultyInfos[difficulty].Incorrect = 0;
+        }
+        DifficultyInfos[difficulty].Correct++;
+    }
+
+    public void AddInCorrect(int difficulty) {
+        if (!DifficultyInfos.ContainsKey(difficulty)) {
+            DifficultyInfos.Add(difficulty, new DifficultyInfo());
+            DifficultyInfos[difficulty].Correct = 0;
+            DifficultyInfos[difficulty].Incorrect = 0;
+        }
+        DifficultyInfos[difficulty].Incorrect++;
+    }
+
+    public int GetCorrect(int difficulty) {
+        if (DifficultyInfos.ContainsKey(difficulty)) {
+            return DifficultyInfos[difficulty].Correct;
+        }
+        return 0;
+    }
+
+    public int GetInCorrect(int difficulty) {
+        if (DifficultyInfos.ContainsKey(difficulty)) {
+            return DifficultyInfos[difficulty].Incorrect;
+        }
+        return 0;
+    }
+}
--- Assets/Scripts/UserInterface/GameInterface.cs
+++ Assets/Scripts/UserInterface/GameInterface.cs
@@ -3,7 +3,12 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class GameInterface : Singleton<GameInterface> {
+    private Animator animator;
+    private bool _isVisable;
+
     public Text isCorrect;
+    public Text corrects;
+    public Text incorrects;
 
     public GameObject menuPanel;
 
@@ -11,22 +16,123 @@ public class GameInterface : Singleton<GameInterface> {
     public Button replayBtn;
     public Button nextBtn;
 
+    public AudioSource audioSource;
+    public AudioClip winClip;
+    public AudioClip loseClip;
+    public AudioClip buttonPressClip;
+
 	// Use this for initialization
 	void Start () {
+        animator = GetComponent<Animator>();
+
+        GameManager.Instance.OnGameStateChange +=
+            OnGameStateChange;
+
+        isCorrect.gameObject.SetActive(false);
+        menuPanel.gameObject.SetActive(false);
+
         previousBtn.onClick.AddListener(OnPreviousClick);
         replayBtn.onClick.AddListener(OnReplayClick);
-        nextBtn.onClick.AddListener(OnNextClick);
+        nextBtn.onClick.AddListener(OnNextClick);        
 	}
 
-    public void OnPreviousClick() {
+    public void UpdateScoreValues() {
+        corrects.text = "Corrects: " + ScoreManager.Instance.GetCorrect(GameManager.Instance.boardSize);
+        incorrects.text = ScoreManager.Instance.GetInCorrect(GameManager.Instance.boardSize) + " :Incorrects";
+    }
 
+    public void OnGameStateChange(GameState state) {
+        if (state == GameState.GameOver) {
+            UpdateScoreValues();
+            isCorrect.gameObject.SetActive(true);
+            menuPanel.gameObject.SetActive(true);
+
+            if (audioSource != null) {
+                audioSource.PlayOneShot(loseClip);
+            }
+
+            isCorrect.text = "Incorrect";
+            if (GameManager.Instance.boardSize <= 2) {
+                previousBtn.gameObject.SetActive(false);
+            } else {
+                previousBtn.gameObject.SetActive(true);
+            }
+            nextBtn.gameObject.SetActive(false);
+            animator.SetTrigger("Show");
+            _isVisable = true;
+        } else if (state == GameState.GameWin) {
+            UpdateScoreValues();
+            isCorrect.gameObject.SetActive(true);
+            menuPanel.gameObject.SetActive(true);
+
+            if (audioSource != null) {
+                audioSource.PlayOneShot(winClip);
+            }
+
+            isCorrect.text = "Correct";
+            if (GameManager.Instance.boardSize <= 2) {
+                previousBtn.gameObject.SetActive(false);
+            } else {
+                previousBtn.gameObject.SetActive(true);
+            }
+            nextBtn.gameObject.SetActive(true);
+            animator.SetTrigger("Show");
+            _isVisable = true;
+        } else if (_isVisable) {
+            animator.SetTrigger("Hide");
+            _isVisable = false;
+        }
+    }
+
+    public void OnPreviousClick() {
+        GameManager.Instance.ModifyBoardSize(-1);
+        GameManager.Instance.ActiveState = GameState.Cinematic;
+        PlayButtonPressAudio();
     }
 
     public void OnReplayClick() {
-
+        GameManager.Instance.ActiveState = GameState.Cinematic;
+        PlayButtonPressAudio();
     }
 
     public void OnNextClick() {
+        GameManager.Instance.ModifyBoardSize(1);
+        GameManager.Instance.ActiveState = GameState.Cinematic;
+        PlayButtonPressAudio();
+    }
 
+    private void PlayButtonPressAudio() {
+        if (audioSource != null) {
+            audioSource.PlayOneShot(buttonPressClip);
+        }
+    }
+
+    public void OnHideComplete() {
+        if (!GameManager.Instance.IsGenerating) {
+            GameManager.Instance.GenerateBoard();
+        }
+        isCorrect.gameObject.SetActive(false);
+        menuPanel.gameObject.SetActive(false);
+    }
+
+    public void OnPlayScoreAnim() {
+        if (GameManager.Instance.ActiveState == GameState.GameOver) {
+            Debug.Log("Play Incorrect Anim");
+            animator.SetTrigger("Incorrect");
+        } else if (GameManager.Instance.ActiveState == GameState.GameWin) {
+            Debug.Log("Play Correct Anim");
+            animator.SetTrigger("Correct");
+        }
+    }
+
+    public void OnChangeScore() {
+        if (GameManager.Instance.ActiveState == GameState.GameOver) {
+            Debug.Log("Adding To Incorrect Score");
+            ScoreManager.Instance.AddInCorrect(GameManager.Instance.boardSize);
+        } else if (GameManager.Instance.ActiveState == GameState.GameWin) {
+            Debug.Log("Adding to Correct Score");
+            ScoreManager.Instance.AddCorrect(GameManager.Instance.boardSize);
+        }
+        UpdateScoreValues();
     }
 }
