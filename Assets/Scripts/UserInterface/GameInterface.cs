f521972b545b20109e20a55c989d6add4d0610d6
Jacob Penner <jkpenner91@gmail.com>
2016-02-17 23:59:23 -0800

    Add Assets for UserInterface

---
+++ Assets/Scripts/UserInterface/GameInterface.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameInterface : Singleton<GameInterface> {
+    public Text isCorrect;
+
+    public GameObject menuPanel;
+
+    public Button previousBtn;
+    public Button replayBtn;
+    public Button nextBtn;
+
+	// Use this for initialization
+	void Start () {
+        previousBtn.onClick.AddListener(OnPreviousClick);
+        replayBtn.onClick.AddListener(OnReplayClick);
+        nextBtn.onClick.AddListener(OnNextClick);
+	}
+
+    public void OnPreviousClick() {
+
+    }
+
+    public void OnReplayClick() {
+
+    }
+
+    public void OnNextClick() {
+
+    }
+}
