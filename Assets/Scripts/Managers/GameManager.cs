0143d97b8eeeb95b01ff3144486f879f3853d6cb
Jacob Penner <jkpenner91@gmail.com>
2016-02-17 20:21:12 -0800

    Create GameManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Singleton<GameManager> {
+    protected GameManager() { }
+
+    public GameObject prefabTileObject;
+    public GameBoard activeGameBoard;
+
+    public int boardSize = 2;
+
+    public void Start() {
+        activeGameBoard.GenerateBoard(5, 5);
+        for (int x = 0; x < 5; x++) {
+            var tile = Instantiate(prefabTileObject).GetComponent<Tile>();
+            tile.SetText("" + (char)((int)('A') + x));
+
+            activeGameBoard.SetTile(x, 0, tile);
+        }
+    }
+
+    public Tile CreateTile() {
+        if (prefabTileObject != null) {
+            var go = GameObject.Instantiate(prefabTileObject);
+            var tile = go.GetComponent<Tile>();
+            if (tile == null) {
+                Debug.LogError("Tile Object Prefab does not have component 'Tile'");
+            }
+            return tile;
+        }
+        return null;
+    }
+}
