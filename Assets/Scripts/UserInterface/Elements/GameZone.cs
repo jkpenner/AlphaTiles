55bb820271a46dd6b10b6cc931f29f766c038acf
Jacob Penner <jkpenner91@gmail.com>
2016-03-03 00:11:54 -0800

    GameZone script controlls the visiblity of the GameBoards

--- Assets/Scripts/UserInterface/Elements/GameZone.cs
+++ Assets/Scripts/UserInterface/Elements/GameZone.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System;
 
 public class GameZone : GameInterfaceElement {
+    public GameBoard start;
+    public GameBoard result;
+
     public override bool IsElementVisible(GameState state) {
         return state == GameState.Active ||
             state == GameState.Checking ||
@@ -17,10 +20,14 @@ public class GameZone : GameInterfaceElement {
 
     public override void OnVisible() {
         base.OnVisible();
+        start.gameObject.SetActive(true);
+        result.gameObject.SetActive(true);
     }
 
     public override void OnHidden() {
         base.OnHidden();
+        start.gameObject.SetActive(false);
+        result.gameObject.SetActive(false);
     }
 
     void Start () {
