10c8931693845a3c58210a7bb8dc7316d3457e42
Jacob Penner <jkpenner91@gmail.com>
2016-03-03 00:13:48 -0800

    Update GameIntro sript to call LoginManager

--- Assets/Scripts/UserInterface/Elements/GameIntro.cs
+++ Assets/Scripts/UserInterface/Elements/GameIntro.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System;
 
@@ -6,12 +7,46 @@ public class GameIntro : GameInterfaceElement {
     public AudioSource audioSource;
     public AudioClip gameIntroEnter;
 
+    public enum IntroState {
+        AttemptingLogin,
+        LoginType,
+        ReadyToPlay,
+    }
+    public IntroState state = IntroState.AttemptingLogin;
+
+    public GameObject tapToPlay;
+    public Button facebookLoginBtn;
+    public Button noLoginBtn;
+
+    public void Start() {
+        LoginManager.Instance.OnInitSuccessEvent += OnInitSuccess;
+        LoginManager.Instance.OnLoginSuccessEvent += OnLoginSuccess;
+        LoginManager.Instance.Initialize();
+
+        facebookLoginBtn.onClick.AddListener(OnFacebookLoginClick);
+        noLoginBtn.onClick.AddListener(OnNoLoginClick);
+    }
+
     public void Update() {
-        if (GameManager.Instance.ActiveState == GameState.Intro && Input.anyKeyDown) {
-            GameManager.SetState(GameState.Cinematic);
-            if (audioSource != null) {
-                audioSource.PlayOneShot(gameIntroEnter);
+        if (state == IntroState.ReadyToPlay) {
+            if (GameManager.Instance.ActiveState == GameState.Intro && Input.anyKeyDown) {
+                GameManager.SetState(GameState.Cinematic);
+                if (audioSource != null) {
+                    audioSource.PlayOneShot(gameIntroEnter);
+                }
             }
+
+            tapToPlay.SetActive(true);
+            facebookLoginBtn.gameObject.SetActive(false);
+            noLoginBtn.gameObject.SetActive(false);
+        } else if (state == IntroState.LoginType) {
+            tapToPlay.SetActive(false);
+            facebookLoginBtn.gameObject.SetActive(true);
+            noLoginBtn.gameObject.SetActive(true);
+        } else {
+            tapToPlay.SetActive(false);
+            facebookLoginBtn.gameObject.SetActive(false);
+            noLoginBtn.gameObject.SetActive(false);
         }
     }
 
@@ -21,6 +56,51 @@ public class GameIntro : GameInterfaceElement {
 
     public override void OnVisible() {
         base.OnVisible();
+
+        if (LoginManager.Instance.loginType == LoginManager.LoginType.NotLoggedIn) {
+            if (!LoginManager.Instance.IsInitialized) {
+                LoginManager.Instance.Initialize();
+            } else {
+                Debug.LogFormat("[{0}]: Login Manager is already Initialized", name);
+            }
+
+            if (!LoginManager.Instance.IsLoggedIn) {
+                // Display Login Options
+            } else {
+                Debug.LogFormat("[{0}]: User in not logged in", name);
+                // Display Tap to Play Message
+            }
+        } else {
+            state = IntroState.ReadyToPlay;
+        }
+    }
+
+    public void OnInitSuccess() {
+        if (!LoginManager.Instance.IsLoggedIn) {
+            state = IntroState.LoginType;
+        }
+    }
+
+    public void OnLoginSuccess() {
+        if (LoginManager.Instance.IsLoggedIn) {
+            state = IntroState.ReadyToPlay;
+            LoginManager.Instance.loginType = LoginManager.LoginType.ActiveFacebook;
+        }
+    }
+
+    public void OnFacebookLoginClick() {
+        if (LoginManager.Instance.IsInitialized) {
+            if (!LoginManager.Instance.IsLoggedIn) {
+                LoginManager.Instance.FacebookLogin();
+            }
+        } else {
+            Debug.LogFormat("[{0}]: Tried to Login to Facebook, but API is not initialized", name);
+        }
+    }
+
+    public void OnNoLoginClick() {
+        LoginManager.Instance.loginType = LoginManager.LoginType.ActiveNoLogin;
+        state = IntroState.ReadyToPlay;
     }
 
     public override void OnHidden() {
